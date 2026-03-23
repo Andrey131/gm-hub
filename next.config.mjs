a7beb57740c81b61f62a66942d7ace5e34c39588
Пользователь <mulitza.an@mail.ru>
2026-03-24 00:33:51 +0300

    configurations for supabase storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["wczupahgsvvufgilxcby.supabase.co"],
+    domains: ["jcnrwiegipjegyintesq.supabase.co"],
   },
 };
 
